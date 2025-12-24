2af7d2177cfeda9db6fc19c983cdfea02deb6479
Uma M <2395a32c6@gprec.ac.in>
2025-12-24 22:49:58 +0530

    pattern3 added

---
+++ pattern3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class pattern3 {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of rows:");
+        int rows=sc.nextInt();
+        System.out.println("Enter number of columns:");
+        int cols=sc.nextInt();
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
