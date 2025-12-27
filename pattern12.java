d513584eb206c94176f4719f16ad3ad7a0cb4734
Uma M <2395a32c6@gprec.ac.in>
2025-12-27 09:13:02 +0530

    added

---
+++ pattern12.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class pattern12 {
+    public static void main(String args[])
+    { 
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter n value: ");
+        int n=sc.nextInt();
+       for(int i=1;i<=n;i++)
+       {
+         for(int j=1;j<=i;j++)
+         {
+            System.out.print(j);
+         }
+         for(int j=1;j<=2*(n-i);j++)
+         {
+            System.out.print(" ");         
+         }
+         for(int j=i;j>=1;j--)
+         {
+            System.out.print(j);
+         }
+         System.out.println();
+       }
+    }
+}
